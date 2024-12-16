b55da063d976674108d5bfea6668c5d8954fbaf4
Hai Tran <haitran@192.168.1.4>
2024-12-16 15:15:41 +0700

    Remove unused dependencies

--- functions/api.js
+++
@@ -1,5 +0,0 @@
-const serverless = require("serverless-http");
-const app = require("../app"); // Import the Express app
-
-// Export the app wrapped for serverless
-module.exports.handler = serverless(app);
