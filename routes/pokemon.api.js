5a2b2791857ef58003d16280a905a4094d977587
Hai Tran <haitran@192.168.1.8>
2024-12-17 10:20:24 +0700

    Update for submit

--- routes/pokemon.api.js
+++ routes/pokemon.api.js
@@ -83,7 +83,6 @@ router.get("/:pokemonId", (req, res, next) => {
     db = JSON.parse(db);
     const { data: pokemons } = db;
     const totalPokemons = pokemons.length;
-
     // Find pokemon by id
     const thisPokemon = pokemons.find((pokemon) => pokemon.id === id);
     // Valid pokemon id
@@ -125,7 +124,78 @@ router.get("/:pokemonId", (req, res, next) => {
 
 router.post("/", (req, res, next) => {
   try {
-    res.status(200).send("post a pokemon");
+    const { name, id, url, types } = req.body;
+    const pokemonId = parseInt(id, 10); // Ép kiểu thành số
+    const filteredTypes = Array.isArray(types)
+      ? types.filter((item) => item && item.trim() !== "")
+      : []; // Make sure types is an array before apply filter method
+    // Check missing data
+    if (!name || !pokemonId || !url || filteredTypes.length === 0) {
+      const exception = new Error(`Missing required data.`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+    // Check types length
+    if (filteredTypes.length > 2) {
+      const exception = new Error(`Pokémon can only have one or two types.`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+    // Check valid pokemon types
+    const validPokemonTypes = [
+      "bug",
+      "dragon",
+      "fairy",
+      "fire",
+      "ghost",
+      "ground",
+      "normal",
+      "psychic",
+      "steel",
+      "dark",
+      "electric",
+      "fighting",
+      "flyingText",
+      "grass",
+      "ice",
+      "poison",
+      "rock",
+      "water",
+    ];
+    const allExist = filteredTypes.every((item) =>
+      validPokemonTypes.includes(item)
+    ); // Validate all item in filteredTypes included in validPokemonTypes
+    if (!allExist) {
+      const exception = new Error(`Pokémon's type is invalid.`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+    // Check if id or name exists in the database
+    let db = fs.readFileSync("db.json", "utf-8");
+    db = JSON.parse(db);
+    const { data: pokemons } = db;
+    const foundPokemon = pokemons.find(
+      (pokemon) => pokemon.id === pokemonId || pokemon.name === name
+    );
+    if (foundPokemon) {
+      const exception = new Error(`The Pokémon already exists.`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+    // Create new pokemon
+    const newPokemon = { id: pokemonId, name, types: filteredTypes, url };
+    // Add new pokemon to pokemons JS object
+    pokemons.push(newPokemon);
+    const newTotalPokemons = pokemons.length;
+    // Add pokemons JS object to db JS object
+    db.data = pokemons;
+    db.totalPokemons = newTotalPokemons;
+    // Convert db JS object to JSON string
+    db = JSON.stringify(db);
+    // Write and save to db.json
+    fs.writeFileSync("db.json", db);
+    // Post send response
+    res.status(200).send(newPokemon);
   } catch (error) {
     next(error);
   }
@@ -140,7 +210,101 @@ router.post("/", (req, res, next) => {
 
 router.put("/:pokemonId", (req, res, next) => {
   try {
-    res.status(200).send("update a pokemon");
+    const allowUpdate = ["name", "url", "types"]; // Cannot edit the id
+    const { pokemonId } = req.params;
+    const id = parseInt(pokemonId, 10); // Ép kiểu thành số
+    const updates = req.body;
+    const updateKeys = Object.keys(updates);
+    // Find update request keys that not allow
+    const notAllow = updateKeys.filter((el) => !allowUpdate.includes(el));
+    if (notAllow.length) {
+      const exception = new Error(`Update field not allow`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+    // Check valid types
+    if (req.body.types) {
+      const types = req.body.types;
+      // Check if types empty
+      const filteredTypes = Array.isArray(types)
+        ? types.filter((item) => item && item.trim() !== "")
+        : []; // Make sure types is an array before apply filter method
+      // Check types length
+      console.log("types", filteredTypes);
+      if (filteredTypes.length === 0) {
+        const exception = new Error(`Pokémon must have at least one type.`);
+        exception.statusCode = 401;
+        throw exception;
+      }
+      if (filteredTypes.length > 2) {
+        const exception = new Error(`Pokémon can only have one or two types.`);
+        exception.statusCode = 401;
+        throw exception;
+      }
+      // Check valid types
+      const validPokemonTypes = [
+        "bug",
+        "dragon",
+        "fairy",
+        "fire",
+        "ghost",
+        "ground",
+        "normal",
+        "psychic",
+        "steel",
+        "dark",
+        "electric",
+        "fighting",
+        "flyingText",
+        "grass",
+        "ice",
+        "poison",
+        "rock",
+        "water",
+      ];
+      const allExist = filteredTypes.every((item) =>
+        validPokemonTypes.includes(item)
+      ); // Validate all item in filteredTypes included in validPokemonTypes
+      if (!allExist) {
+        const exception = new Error(`Pokémon's type is invalid.`);
+        exception.statusCode = 401;
+        throw exception;
+      }
+    }
+    // Get database
+    let db = fs.readFileSync("db.json", "utf-8");
+    db = JSON.parse(db);
+    const { data: pokemons } = db;
+    // Check name exists
+    if (req.body.name) {
+      const name = req.body.name;
+      // Filter out this pokemon by id before find by name
+      const otherPokemons = pokemons.filter((pokemon) => pokemon.id !== id);
+      const foundPokemonName = otherPokemons.find(
+        (pokemon) => pokemon.name === name
+      );
+      if (foundPokemonName) {
+        const exception = new Error(`The Pokémon name already exists.`);
+        exception.statusCode = 401;
+        throw exception;
+      }
+    }
+    // Find book by id
+    const targetIndex = pokemons.findIndex((pokemon) => pokemon.id === id);
+    if (targetIndex < 0) {
+      const exception = new Error(`Pokemon not found`);
+      exception.statusCode = 404;
+      throw exception;
+    }
+    // Start update pokemon
+    const updatedPokemon = { ...db.data[targetIndex], ...updates };
+    db.data[targetIndex] = updatedPokemon;
+    // Convert db JS object to JSON string
+    db = JSON.stringify(db);
+    // Write and save to db.json
+    fs.writeFileSync("db.json", db);
+    // Update response
+    res.status(200).send(updatedPokemon);
   } catch (error) {
     next(error);
   }
@@ -155,7 +319,30 @@ router.put("/:pokemonId", (req, res, next) => {
 
 router.delete("/:pokemonId", (req, res, next) => {
   try {
-    res.status(200).send("delete a pokemon");
+    const { pokemonId } = req.params;
+    const id = parseInt(pokemonId, 10); // Ép kiểu thành số
+    let db = fs.readFileSync("db.json", "utf-8");
+    db = JSON.parse(db);
+    const { data: pokemons } = db;
+    // Find pokemon by id
+    const targetIndex = pokemons.findIndex((pokemon) => pokemon.id === id);
+    // Valid pokemon id
+    if (targetIndex < 0) {
+      const exception = new Error(`Pokemon not found`);
+      exception.statusCode = 404;
+      throw exception;
+    }
+    // Filter db pokemons object (this means remove the matched item from db)
+    const newPokemons = pokemons.filter((pokemon) => pokemon.id !== id);
+    const newTotalPokemons = newPokemons.length;
+    db.data = newPokemons;
+    db.totalPokemons = newTotalPokemons;
+    // Convert db JS object to JSON string
+    db = JSON.stringify(db);
+    // Write it back and save to db.json
+    fs.writeFileSync("db.json", db);
+    // Delete send response
+    res.status(200).send({});
   } catch (error) {
     next(error);
   }
